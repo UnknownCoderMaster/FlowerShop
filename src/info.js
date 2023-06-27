09cb0c968d173b0af691bc3cc44f6e793dc60d27
UnknownCoderMaster <110689681+UnknownCoderMaster@users.noreply.github.com>
2023-06-27 19:13:51 +0500

    Initial

---
+++ src/info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Navbar from "./navbar";
+
+function Info(props) {
+
+
+
+    return (
+        <div>
+            <Navbar/>
+
+        </div>
+    );
+}
+
+export default Info;
